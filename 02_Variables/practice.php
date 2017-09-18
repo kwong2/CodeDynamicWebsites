7c3670810b83f45f37c93abcf1910b732a4943cc
Kevin Wong <k.wong4@yahoo.com>
2017-09-18 18:14:41 -0400

    created 2nd practice file with variable

---
+++ 02_Variables/practice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Variables</title>
+	</head>
+	<body>
+    <?php 
+    $var = "hello";
+    $car = "world";
+
+    print ("As this is my first variables practice, I thought I should print out $var $car");
+    
+    ?>
+	</body>
+</html>
