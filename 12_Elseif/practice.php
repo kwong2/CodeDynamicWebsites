fa38d754ae4686c51525a6bd9e3f98a1ec244382
Kevin Wong <k.wong4@yahoo.com>
2017-09-20 23:06:52 -0400

    completed practice for lesson 12, ELSIF executed

--- 12_Elseif/practice.php
+++ 12_Elseif/practice.php
@@ -1,17 +1,22 @@
 <?php
 	
 	// Constants
-
+	define ("TITLE", "Lesson 12, The ELSEIF");
 	
 	// Custom Variables
-
+	$name = "Kevin Wong";
+	$lesson_number = 12;
+	$spain = "Spanish";
+	$england = "English";
+	$france = "french";
+	$germany = "german";
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?> </title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,20 +25,29 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_number; ?> : <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
+				<?php 
+					if ($spain == "English"){
+						echo "No habla ingles";
+					} elseif ($spain == "Spanish") {
+						echo "Si Si Senor";
+					} else {
+						echo "WRONG";
+					};
+				?>
+
 			</div><!-- end sandbox -->
 			
 			<a href="index.php" class="button">Back to the lecture</a>
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?>  - <?php echo $name ; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
