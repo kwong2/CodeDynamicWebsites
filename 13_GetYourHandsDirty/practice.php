4506f8102e8df863e346a7ec1cc0cf04a0871b99
Kevin Wong <k.wong4@yahoo.com>
2017-09-21 17:15:33 -0400

    Completed Lesson 13 with new variables and if, elseif, else

--- 13_GetYourHandsDirty/practice.php
+++ 13_GetYourHandsDirty/practice.php
@@ -1,16 +1,21 @@
 <?php
 	
 	// Constants
-	
+	define ("TITLE", "If, Else, Elsif. Yeah it keeps goin on.");
 	
 	// Custom Variables
+	$name = "Kevin Wong";
+	$lesson_num = 13;
 
+	$a = "the allies";
+	$b = "the germans";
+	$c = "base";
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE ;?> </title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -19,20 +24,28 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Get Your Hands Dirty: <small><!-- TITLE --></small></h1>
+			<h1>Get Your Hands Dirty: <small><?php echo TITLE ;?> </small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
+				<?php
+					if ($c == "the germans") {
+						echo "<p> Guys look out, the Germans have come out with a new sham wow. </p>";
+					} elseif ($c == "the allies") {
+						echo "Man them Allies are using their Spitfires to make jaguars now.";
+					} else {
+						echo "Yeah it appears as the c actually is $c";
+					}
+				?>
 			</div><!-- end sandbox -->
 			
 			<a href="index.php" class="button">Back to the lecture</a>
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?> - <?php echo $name ?> </small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
