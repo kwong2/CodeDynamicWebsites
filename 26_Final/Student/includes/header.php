eb51c6f373b1ce4ccb29b45e476af9f262c8771a
Kevin Wong <k.wong4@yahoo.com>
2017-09-25 17:19:45 -0400

    created header.php and pasted from index.php

---
+++ 26_Final/Student/includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>China Hollywood</title>
+    <link rel= "stylesheet" href="../../assets/styles.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body id="final-example">
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div class = "wrapper">
+
+      <div id="banner">
+        <a href="index.php" title="Return to Home">
+          <img src="img/banner.png" alt="China Hollywood">
+
+      </div>
+    
+      <div id="nav">
+        <ul>
+            <!-- need to correct links -->
+          <li><a href="index.php">Home</a></li>
+          <li><a href="menu.php">Menu</Menu:c></a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </div>
