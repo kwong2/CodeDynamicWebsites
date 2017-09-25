436647cca0a6dee33af651e38542840e423f6971
Kevin Wong <k.wong4@yahoo.com>
2017-09-25 17:24:07 -0400

    created footer and pasted from index

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,34 @@
+
+      <div class="content">
+        <div id="footer" class="cf">
+          <div class="column three">
+            <strong>Phone</strong>
+            954-966-9600
+          </div>
+
+          <div class="column three">
+            <strong>Location</strong>
+            3605 Hollywood Blvd. <br>
+            Hollywood, FL 33024
+          </div>
+
+          <div class="column three">
+            <strong>Hours</strong>
+            <em>Monday - Saturday</em><br>
+            10:00 AM to 10:00 PM<br>
+            <br>
+            <em>Closed on Sundays</em><br>
+            
+
+          </div>
+          
+        </div>
+      </div>
+
+
+    </div>
+
+    
+    
+  </body>
+</html>
