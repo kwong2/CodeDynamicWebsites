42176dca450f4cc4148458389804daa03ba034e4
Kevin Wong <k.wong4@yahoo.com>
2017-10-01 12:57:49 -0400

    keeping original styling

--- 26_Final/Student/includes/header.php
+++ 26_Final/Student/includes/header.php
@@ -12,8 +12,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php echo TITLE ?> </title>
+    <!-- original style.css -->
     <link rel= "stylesheet" href="assets/styles.css">
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
+
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
